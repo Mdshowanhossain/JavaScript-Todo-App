94fec85ab16f2ee7c0471c91e5da6b3bc01b6192
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-06-24 20:49:17 +0600

    todo-App

---
+++ script.js
@@ -0,0 +1,70 @@
+// var selections 
+
+let inputSelection = document.getElementById("add");
+
+let IncompleteItems = document.getElementById("items");
+
+let completeItems = document.getElementById("complete ul");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("addTask").addEventListener("click", function () {
+// const checkBox = document.getElementById("add").value;
+// const list = document.getElementById("task-list");
+// const newlist = document.getElementById("task-list").innerText = checkBox;
+// document.getElementById("task-list").appendChild(add.value);
+// })
+
+//  1.  nextElementSibling
+// 2.   previousElementSibling
+
+// const parent = document.getElementById("item");
+// const children = parent.previousElementSibling;
+// parent.style.color = 'red';
+// parent.style.color = 'yellow'
+// console.log(parent)
+
+// createElement
+
+// 1. insertBefore
+// 2.appendChild = ar moddha obbosoy akta html element deta hoba. aktaay append korbaa last'A
+// 3. append= ar moddha amra jekono text dhukay deta parboo.Ak sthaa onk guloo dhukaay deta paboo problm neei
+
+
+// const firstElement = document.createElement('div');
+// firstElement.className = "task"
+// firstElement.setAttribute("id", "task")
+
+// const incomplete = document.getElementById("incomplete");
+
+// const h2Element = incomplete.elementsByTagName("ul");
+
+// incomplete.insertBefore(firstElement, h2Element)
+
